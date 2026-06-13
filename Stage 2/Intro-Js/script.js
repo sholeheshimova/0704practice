338692b6fd9e9c434830bc2e3bdcb1281f932bf4
Şölə Həşimova <solhsimova@gmail.com>
2026-06-13 21:14:35 +0400

    first commit

---
+++ Stage 2/Intro-Js/script.js
@@ -0,0 +1,50 @@
+// let childrenNum = 2;
+// let partnerName = "Shola";
+// let loc = "Azerbaijan";
+// let job = "Devoloper";
+
+
+// let result = "You will be "  +  job +  " in "  +  loc  +  " and married to "  +  partnerName  +  " with "  +  childrenNum  +  " kids";
+
+// console.log(result);
+
+
+
+
+
+// let birthYear = 2005;
+// let futureYear = 2045;
+
+
+// let age1 = futureYear - birthYear;
+// let age = age1 - 1;
+// console.log("I will be either " + age + " or " + age1 + " in " + futureYear);
+
+
+
+
+// let currentAge = 21;
+// let maximumAge = 101;
+
+// let averageCandy = 6;
+
+// let resultCandy = (maximumAge - currentAge) * 6 * 365
+// console.log(resultCandy);
+
+
+
+// let pi = 3.14
+// let radius = 3;
+
+// let circumference = 2 * pi * radius
+// let area = pi * (radius**2)
+
+// console.log("the circumference is " + circumference); 
+// console.log("the area is " + area);  
+
+
+
+
+
+
+
