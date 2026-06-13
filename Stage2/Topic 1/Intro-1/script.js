0d0c2472ff492e2ea7e9792fc2e6ebb6c0bb37ab
Şölə Həşimova <solhsimova@gmail.com>
2026-06-13 21:28:57 +0400

    celci to faranheit

---
+++ Stage2/Topic 1/Intro-1/script.js
@@ -0,0 +1,8 @@
+let celci = 32;
+let fahranheit = 74;
+
+let celciToFaranh = (9 / 5) * celci + 32;
+let faranhToCelci = (5 / 9) * (fahranheit - 32);
+
+console.log(`${faranhToCelci} C is ${celciToFaranh} F`);
+console.log(`${celciToFaranh} F is ${faranhToCelci} C`);
